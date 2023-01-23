bf7bb845bf6344eef20bda46d340b484d024be48
adins <adins@hotmail.se>
2023-01-23 10:26:41 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000
+const https = require('https')
+
+const instrument = { instruments: [
+    {"name": "Guitar", "kind": "String Instrument"},
+    {"name": "Piano", "kind": "PlingPlong"},
+    {"name": "Violin", "kind": "String Instrument"}
+]}
+
+app.get("/joke", (req,res) => {
+    https.get("https://api.chucknorris.io/jokes/random", (response) => {
+        response.on('data', (data) => {
+            res.send(JSON.parse(data))
+        })
+
+    }).on("error", (err) => {
+        console.log("There was an error " + err.message)
+    })
+})
+
+app.get("/instruments", (req, res) => {
+    res.send(instrument)
+})
+
+app.get("/", (req,res) => {
+    res.sendFile(__dirname + "/index.html")
+})
+
+
+app.listen(PORT, () => {
+    console.log("Listening to Port: " + PORT)
+})
